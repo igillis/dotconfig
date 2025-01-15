13cd97c9745c40877d44abd250f536649ebde62d
igillis-clara <ian@clarahomecare.com>
2025-01-15 12:58:11 -0500

    Linting improvements

--- nvim-lazyvim/lua/plugins/conform.lua
+++ nvim-lazyvim/lua/plugins/conform.lua
@@ -1,8 +1,20 @@
 return {
   "stevearc/conform.nvim",
   opts = {
+    formatters = {
+      htmlbeautifier = {
+        prepend_args = { "-b", "1" },
+      },
+      erb_lint = {
+        stdin = false,
+        tmpfile_format = ".conform.$RANDOM.$FILENAME",
+        command = "bundle",
+        args = { "exec", "erblint", "--autocorrect", "$FILENAME" },
+      },
+    },
     formatters_by_ft = {
-      eruby = { "htmlbeautifier" },
+      html = { "htmlbeautifier" },
+      eruby = { "htmlbeautifier", "erb_lint" },
       ruby = { "standardrb" },
       markdown = { "markdownlint" },
       yaml = { "yamlfix" },
